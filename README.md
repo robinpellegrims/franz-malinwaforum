edaf9dac05a9cf5074f0efea26f4f9670e850f92
Robin Pellegrims <6053571+RobinPel@users.noreply.github.com>
2019-09-26 16:52:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Malinwaforum for Franz
+
+This is an unofficial [Malinwaforum](https://malinwaforum.be) recipe for [Franz](https://meetfranz.com/).
+
+For installation, visit the [Franz documentation](https://github.com/meetfranz/plugins/blob/master/docs/integration.md#installation).
---
+++ webview.js
@@ -0,0 +1,15 @@
+module.exports = Franz => {
+
+    getMessages = () => {
+        const text = document.querySelectorAll("div.pagination")[0].innerText;
+        const pattern = /Er zijn (\d+) resultaten gevonden/i
+        const regexResult = pattern.exec(text);
+
+        const counter = parseInt(regexResult[1]);
+
+        Franz.setBadge(counter)
+    };
+
+    Franz.loop(getMessages);
+
+}
