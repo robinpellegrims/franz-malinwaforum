fa3a87fcbd4ecaa0a0ca2c74388aa49bdf5723ad
robinpel <robin.pellegrims@gmail.com>
2019-09-26 23:37:22 +0200

    fix for 1 result found

--- webview.js
+++ webview.js
@@ -2,7 +2,7 @@ module.exports = Franz => {
 
     getMessages = () => {
         const text = document.querySelectorAll("div.pagination")[0].innerText;
-        const pattern = /Er zijn (\d+) resultaten gevonden/i
+        const pattern = /Er (?:is|zijn) (\d+) (?:resultaat|resultaten) gevonden/i
         const regexResult = pattern.exec(text);
 
         const counter = parseInt(regexResult[1]);
